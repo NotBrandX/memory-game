8c321296f9adc0feb0e4a4bcace7be65ad100b37
brandondoris <brandonmdoris@yahoo.com>
2022-01-19 20:29:21 -0500

    created cards and background

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const getData = () => [
 	{ imgSrc: './images/muse.jpeg', name: 'muse' },
 	{ imgSrc: './images/nirvana.jpeg', name: 'nirvana' },
 	{ imgSrc: './images/nofx.jpg', name: 'nofx' },
-	{ imgSrc: './images/vulfpeck.jpeg', name: 'vulfpeck' },
+	
 	{ imgSrc: './images/wutang.jpg', name: 'wu tang clan' },
 	{ imgSrc: './images/cheaptrick.jpg', name: 'cheap trick' },
 	{ imgSrc: './images/elviscostello.jpg', name: 'elvis costello' },
@@ -24,17 +24,34 @@ const getData = () => [
 	{ imgSrc: './images/muse.jpeg', name: 'muse' },
 	{ imgSrc: './images/nirvana.jpeg', name: 'nirvana' },
 	{ imgSrc: './images/nofx.jpg', name: 'nofx' },
-	{ imgSrc: './images/vulfpeck.jpeg', name: 'vulfpeck' },
 	{ imgSrc: './images/wutang.jpg', name: 'wu tang clan' },
 ];
 
 // Randomize
 const randomize = () => {
     const cardData = getData();
-
     cardData.sort(() => Math.random() -0.5);
-    console.log(cardData);
+    return cardData;
 }
 
-randomize();
+// Card Generator Function
+const cardGenerator = () => {
+    const cardData = randomize();
+    // Generate HTML
+    cardData.forEach(item => {
+        const card =document.createElement('div');
+        const face =document.createElement('img');
+        const back =document.createElement('div');
+        card.classList = 'card';
+        face.classList = 'face';
+        back.classList = 'back';
+        // Attach the info to the cards
+        face.src = item.imgSrc;
+        // Attach the cards to the section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+    })
+};
 
+cardGenerator()
